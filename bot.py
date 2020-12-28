e09f8a96274bb7d948bf473af94d8c0b0c4f9980
Romsick <roman.torressalmanov@sainsburys.co.uk>
2020-12-28 19:51:50 +0000

    Delete test

--- bot.py
+++ bot.py
@@ -20,8 +20,6 @@ posts = reddit.subreddit('gtaonline').search(post_title_search, sort='new', time
 
 yesterday_timestamp = (datetime.utcnow() - timedelta(days=1)).timestamp()
 
-webhook = DiscordWebhook(url=webhook_url, content="TEST")
-webhook.execute()
 for post in posts:
     # If posted by user we want and in the last 24h
     if post.author == post_author and post.created_utc > yesterday_timestamp:
