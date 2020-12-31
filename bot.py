3e50e18726b7c022e2758a0b1a1541ed9f23b300
Romsick <roman.torressalmanov@sainsburys.co.uk>
2020-12-31 12:13:10 +0000

    Change logic to retrieve post

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import praw
 import json
 import os
+import re
 from datetime import datetime, timedelta
 from discord_webhook import DiscordWebhook
 
@@ -12,7 +13,7 @@ webhook_id = os.environ.get("WEBHOOK_ID")
 webhook_secret = os.environ.get("WEBHOOK_SECRET")
 webhook_url = f"https://discord.com/api/webhooks/{webhook_id}/{webhook_secret}"
 
-post_author = "Call_Me_Tsuikyit"
+# post_author = "Call_Me_Tsuikyit"
 post_title_search = "Weekly GTA Online Bonuses"
 
 reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, user_agent=user_agent)
@@ -21,10 +22,8 @@ posts = reddit.subreddit('gtaonline').search(post_title_search, sort='new', time
 yesterday_timestamp = (datetime.utcnow() - timedelta(days=1)).timestamp()
 
 for post in posts:
-    # If posted by user we want and in the last 24h
-    if post.author == post_author and post.created_utc > yesterday_timestamp:
+    # If title starts with date and posted in the last 24h
+    if re.match(r"^\d{2}\/\d{2}\/\d{4}.*$", post.title) and post.created_utc > yesterday_timestamp:
+        print(f"{post.title} -- {post.author}")
         webhook = DiscordWebhook(url=webhook_url, content=post.selftext)
         webhook.execute()
-
-
-    
